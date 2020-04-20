031b9e3fe5675ad8101e3342642d27b17eaa6418
jose-luis.cafferata.external@axa.com <jlcafferata@macags-40692.home>
2020-04-20 08:38:26 +0200

    Cleaning directory tree and coding

--- src/store/actions/common/manageError.js
+++ src/store/actions/common/manageError.js
@@ -1,42 +1,40 @@
-import notifier from '../../utils/notifier'
-import { messageTypes } from '../../../constants'
-import isEmpty from '../../../utils/isEmpty'
+import notifier from '../../utils/notifier';
+import isEmpty from '../../../utils/isEmpty';
 
 const manageError = ({ error = {}, actionType }) => (dispatch) => {
-  const { response = {}, message: errorMessage } = error || {}
+  const { response = {}, message: errorMessage } = error || {};
   const defaultErroConfig = {
     autoClose: false,
-    className: messageTypes.error,
-  }
+  };
 
   if (!error) {
-    console.log('unhandled error')
+    console.log('unhandled error');
   }
 
   if (errorMessage && isEmpty(response)) {
-    return notifier({ message: errorMessage, ...defaultErroConfig })
+    return notifier({ message: errorMessage, ...defaultErroConfig });
   }
 
   if (actionType) {
     return dispatch({
       type: actionType,
       payload: response.data ? response.data : response,
-    })
+    });
   }
 
   if (typeof error === 'object' && !Array.isArray(error)) {
-    const { status, data: { message: extendedMessage } = {} } = response || {}
+    const { status, data: { message: extendedMessage } = {} } = response || {};
     const messageToNotify = status
       ? `messages.code.error.${status}`
-      : 'messages.general.error'
+      : 'messages.general.error';
     notifier({
       message: messageToNotify,
       ...defaultErroConfig,
       ...(status && { extendedMessage }),
-    })
+    });
   } else {
-    notifier({ message: error, ...defaultErroConfig })
+    notifier({ message: error, ...defaultErroConfig });
   }
-}
+};
 
-export default manageError
+export default manageError;
